f0f5350b4c6471e20bd6ca65c8bafcdffd55e5f4
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-04-18 00:23:55 -0700

    set up ESLint and update code to use ES6 modules

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express') //express module
+import dotenv from 'dotenv';
+import express from 'express'; //express module
 const app = express(); // create an express app
-
-const cors = require('cors');
-const mongoose = require("mongoose"); //allows all cross-origin requests to access the resources
+import mongoose from "mongoose";
+import cors from 'cors'; //allows all cross-origin requests to access the resources
 app.use(cors()); //configure allowed domain/origin later
 // app.use(cors({origin: process.env.frontendURL, credentials: true}));
 
 app.use(express.urlencoded({extended: true})); //parse incoming request bodies with URL-encoded data by the client
 app.use(express.json()); // parse request bodies that are in JSON format
+dotenv.config();
 
 const port = 3000 || process.env.PORT;
 
