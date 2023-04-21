b9be8fa51deb0c484c6a3039f40c58ce4c1829d3
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-04-21 01:13:41 -0700

    default categories for users

---
+++ server/Utils/categoryUtils.js
@@ -0,0 +1,38 @@
+import CategoryModel from '../models/CategoryModel';
+
+const defaultCategories = [
+
+  // expense categories
+  { name: 'Rent' },
+  { name: 'Utilities (Electricity, Water, Gas, Internet' },
+  { name: 'Groceries' },
+  { name: 'Food & drinks' },
+  { name: 'Shopping' },
+  { name: 'Entertainment' },
+  { name: 'Health/Medical' },
+  { name: 'Travel' },
+  { name: 'Gas' },
+  { name: 'Miscellaneous' },
+
+  // income categories
+  { name: 'Salary' },
+  { name: 'Bonus' },
+  { name: 'Gift' },
+  { name: 'Investment' },
+
+];
+
+const createDefaultCategories = async (user) => {
+  const categoryPromises = defaultCategories.map((category) => {
+    const newCategory = new CategoryModel({
+      ...category,
+      user: user.userId,
+    });
+    return newCategory.save();
+  });
+
+  // waiting for all the categories to be saved to the database before continuing
+  await Promise.all(categoryPromises);
+};
+
+export default createDefaultCategories;
