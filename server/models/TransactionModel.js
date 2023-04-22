5da9e4845908cb4d224c708570785ebb15ac82b2
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-04-21 19:38:00 -0700

    add goal field and index to improve query performance

--- server/models/TransactionModel.js
+++ server/models/TransactionModel.js
@@ -7,6 +7,7 @@ const transactionSchema = new mongoose.Schema(
     account: { type: mongoose.Schema.Types.ObjectId, ref: 'account', required: true },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'category', required: true },
     budget: { type: mongoose.Schema.Types.ObjectId, ref: 'budget' },
+    goal: { type: mongoose.Schema.Types.ObjectId, ref: 'goal' },
     type: { type: String, enum: ['income', 'expense'], required: true },
     amount: { type: Number, required: true },
     description: { type: String },
@@ -16,6 +17,11 @@ const transactionSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+// create indexes for the transaction schema to improve query performance
+transactionSchema.index({ type: 1, userId: 1 }); // compound index based on type and userId
+transactionSchema.index({ userId: 1, category: 1 }); // compound index based on userId and category
+transactionSchema.index({ userId: 1 }); // index on userId
 
 const TransactionModel = mongoose.model('transaction', transactionSchema);
 export default TransactionModel;
