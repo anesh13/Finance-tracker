1ea7a33d99f7cdc4ce4357e3464ef11bb0af525b
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-05-04 03:31:17 -0700

    change Link to NavLink

--- client/src/components/leftBar/LeftBar.jsx
+++ client/src/components/leftBar/LeftBar.jsx
@@ -1,57 +1,48 @@
 import "./leftBar.scss";
-import { Link } from 'react-router-dom';
+import SettingsIcon from "@mui/icons-material/Settings";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SportsScoreTwoToneIcon from "@mui/icons-material/SportsScoreTwoTone";
+import SavingsOutlinedIcon from "@mui/icons-material/SavingsOutlined";
+import AttachMoneySharpIcon from "@mui/icons-material/AttachMoneySharp";
 
-import SettingsIcon from '@mui/icons-material/Settings';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import SportsScoreTwoToneIcon from '@mui/icons-material/SportsScoreTwoTone';
+import { NavLink } from "react-router-dom";
 
-import SavingsOutlinedIcon from '@mui/icons-material/SavingsOutlined';
-import AttachMoneySharpIcon from '@mui/icons-material/AttachMoneySharp';
 const LeftBar = () => {
-
-
     return (
         <div className="leftBar">
             <div className="container">
-
                 <div className="menu">
-
-                    <Link to="/" style={{ textDecoration: "none" }}>
+                    <NavLink to="/" style={{ textDecoration: "none" }}>
                         <div className="item">
                             <HomeOutlinedIcon />
                             <span>Dashboard</span>
-
                         </div>
-                    </Link>
-                    <Link to="/transaction" style={{ textDecoration: "none" }}>
+                    </NavLink>
+                    <NavLink to="/transaction" style={{ textDecoration: "none" }}>
                         <div className="item">
                             <AttachMoneySharpIcon />
                             <span>Transaction</span>
                         </div>
-                    </Link>
-
-                    <Link to="/budget" style={{ textDecoration: "none" }}>
+                    </NavLink>
+                    <NavLink to="/budget" style={{ textDecoration: "none" }}>
                         <div className="item">
                             <SavingsOutlinedIcon />
                             <span>Budget</span>
                         </div>
-                    </Link>
-
-                    <Link to="/goal" style={{ textDecoration: "none" }}>
+                    </NavLink>
+                    <NavLink to="/goal" style={{ textDecoration: "none" }}>
                         <div className="item">
                             <SportsScoreTwoToneIcon />
                             <span>Goal</span>
                         </div>
-                    </Link>
-                    <Link to="/settings" style={{ textDecoration: "none" }}>
+                    </NavLink>
+                    <NavLink to="/settings" style={{ textDecoration: "none" }}>
                         <div className="item">
-
-                            < SettingsIcon />
-                            <span>Settings </span>
+                            <SettingsIcon />
+                            <span>Settings</span>
                         </div>
-                    </Link>
+                    </NavLink>
                 </div>
-
             </div>
         </div>
     );
